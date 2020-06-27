f2f4df9975abec6c94d187b233b9dbb91830e18e
Cory <cory.forsyth@gmail.com>
2020-06-27 12:44:16 -0400

    initial client and server

---
+++ client.js
@@ -0,0 +1,40 @@
+const PORT = 2222;
+var buffer = require('buffer');
+var udp = require('dgram');
+
+// creating a client socket
+var client = udp.createSocket('udp4');
+
+//buffer msg
+var data = Buffer.from('siddheshrane');
+
+client.on('message', function (msg, info) {
+  console.log('Data received from server : ' + msg.toString());
+  console.log(
+    'Received %d bytes from %s:%d\n',
+    msg.length,
+    info.address,
+    info.port
+  );
+});
+
+//sending msg
+client.send(data, PORT, 'localhost', function (error) {
+  if (error) {
+    client.close();
+  } else {
+    console.log('Data sent !!!');
+  }
+});
+
+var data1 = Buffer.from('hello');
+var data2 = Buffer.from('world');
+
+//sending multiple msg
+client.send([data1, data2], PORT, 'localhost', function (error) {
+  if (error) {
+    client.close();
+  } else {
+    console.log('Data sent !!!');
+  }
+});
